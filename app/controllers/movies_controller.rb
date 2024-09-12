46fafd846b50f0ee701188474ef3b4360d142b33
黃冠儒 <huangguanru@huangguanrudeMacBook-Air.local>
2024-09-11 22:23:14 -0500

    Fix NoMethodError by defining sortable method

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -73,6 +73,24 @@ class MoviesController < ApplicationController
       @movies = Movie.order("#{sort_column} #{sort_direction}")
     end
   end
+
+  # app/controllers/movies_controller.rb
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @movies = Movie.order("#{sort_column} #{sort_direction}")
+  end
+
+  private
+
+  def sort_column
+    Movie.column_names.include?(params[:sort]) ? params[:sort] : "title"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
+  end
+
   
 
   private
--- app/helpers/movies_helper.rb
+++ app/helpers/movies_helper.rb
@@ -1,2 +1,8 @@
 module MoviesHelper
+   # Helper to generate sortable column links
+   def sortable(column, title = nil)
+    title ||= column.titleize
+    direction = (column == params[:sort] && params[:direction] == "asc") ? "desc" : "asc"
+    link_to title, { sort: column, direction: direction }
+  end
 end
